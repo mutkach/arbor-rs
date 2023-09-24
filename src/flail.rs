a40d59789b7526cfa129a5ee6f02e1ca5f3051ba
mutkach <n.a.savelyev@gmail.com>
2023-09-24 18:23:04 +0400

    initial commit

--- src/flail.rs
+++ src/flail.rs
@@ -1,16 +1,34 @@
-use rapier2d::prelude::{RigidBodyPosition, RigidBodyBuilder};
+use rapier2d::prelude::{RigidBodyPosition, RigidBodyBuilder, ColliderBuilder, Collider, Ball, ColliderSet, vector, point, RigidBodySet, RigidBody, RigidBodyHandle, PrismaticJointBuilder, MultibodyJoint, RopeJoint, RevoluteJointBuilder, RopeJointBuilder};
+use nalgebra::{Point2};
+
 
 
 pub struct Flail<'a> {
-    position: Point2<f32>,
+    position : Point2<f32>,
     player_position: &'a Point2<f32>,
-    handle: RigidBodyHandle,
-    joint: RopeJoint,
+    handle : RigidBodyHandle,
+    joint : RopeJoint,
 }
 
 impl Flail <'_> {
-    pub fn new<'a>(player_position: &'a Point2<f32>) -> Flail <'a> {
+    pub fn new<'a>(player_position : &'a Point2<f32>, handle : RigidBodyHandle) -> Flail <'a>{
         let position = *player_position;
-        
+        let rigid_body = RigidBodyBuilder::fixed().translation(vector![0.0, 0.0]).build();
+        let collider = ColliderBuilder::ball(1.0).build();
+        let joint = RopeJointBuilder::new().local_anchor1(point![0.0, 0.0]).limits([0.0,1.0]).build();
+
+        Flail {
+            position,
+            player_position,
+            joint,
+            handle,
+        }
+
+    }
+
+
+    pub fn update_position(&mut self, time_delta : f32) {
+
+
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,13 +1,28 @@
+use std::ops::AddAssign;
 
+use rapier2d::prelude::{vector, RigidBodySet, RigidBody, Ball, ColliderSet, QueryPipeline, QueryFilter, RigidBodyBuilder};
+use rapier2d::control::{KinematicCharacterController};
+use nalgebra::{Vector2};
 
-
-#[derive(Clone, Copy, Debug)]
-        
-struct Player {
-    x: u8,
-    y: u8,
-    color: String
+pub struct Player {
+    position : Vector2<f32>,
+    shape : Ball,
+    handle : RigidBodyHandle,
+    controller : KinematicCharacterController,
 }
 
 impl Player {
+    pub fn new(position : Vector2<f32>, radius : f32) -> Player {
+        Player {
+            position,
+            shape : Ball::new(radius),
+            body : RigidBodyBuilder::new().build();
+            controller : KinematicCharacterController::default(),
+
+        }
+    }
+
+    pub fn move_player(&mut self, desired_movement : Vector2<f32>){
+        self.position.add_assign(desired_movement);
+    }
 }
